cfeed3607da8d24b3db171606e87436f39bc97de
Shan <du@g-angle.co.jp>
2025-06-09 16:00:36 +0800

    theme setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return <div>
+    <h1 className="text-3xl font-bold underline text-center mt-10 text-violet-300">
+      Hello, world!
+    </h1>
+    <p className="text-center mt-4 font-circular-web">
+      This is a simple React application styled with Tailwind CSS.
+    </p>
+  </div>;
+};
+
+export default App;
