8f4825a03025070eb87c6f6f8b53eef574162a00
Shannen <nenshadu@gmail.com>
2025-06-10 21:36:41 +0800

    feat: about section

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AnimatedTitle = () => {
+  return (
+      <div className='mt-5 text-center tex-4xl uppercase leading-[0.8] md:text-text-[6rem]'>
+          Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure
+      </div>  )
+}
+
+export default AnimatedTitle
